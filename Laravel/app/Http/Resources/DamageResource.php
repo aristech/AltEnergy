053a50d46c7424fd9c66e9c37bc2321b8c1caff3
GManentis <manentis.gerasimos@outlook.com>
2019-10-18 13:28:35 +0300

    Modified DamageResource

--- Laravel/app/Http/Resources/DamageResource.php
+++ Laravel/app/Http/Resources/DamageResource.php
@@ -16,7 +16,7 @@ class DamageResource extends JsonResource
     public function toArray($request)
     {
 
-            return
+        return
             [
                 "id" => $this->id,
                 "damage_type" => $this->type['name'],
@@ -37,9 +37,9 @@ class DamageResource extends JsonResource
                 "client_firstname" => $this->client['firstname'],
                 "client_address" => $this->client['address'],
                 "client_phone" =>  $this->when(true, function () {
-                    if($this->client['telephone'] != null) return $this->client['telephone'];
-                    if($this->client['telephone2'] != null) return $this->client['telephone2'];
-                    if($this->client['mobile'] != null) return $this->client['mobile'];
+                    if ($this->client['telephone'] != null) return $this->client['telephone'];
+                    if ($this->client['telephone2'] != null) return $this->client['telephone2'];
+                    if ($this->client['mobile'] != null) return $this->client['mobile'];
                 }),
                 "manufacturer_id" => $this->manufacturer_id,
                 "manufacturer" => $this->device['mark']['manufacturer']['name'],
@@ -51,107 +51,73 @@ class DamageResource extends JsonResource
                 "comments" => $this->comments,
                 "appointment_start" => $this->appointment_start,
                 "appointment_end" => $this->appointment_end,
-                "techs" => $this->when(true,function()
-                {
+                "techs" => $this->when(true, function () {
                     $technicians = array();
-                    if($this->techs == null)
-                    {
+                    if ($this->techs == null) {
                         return $technicians;
                     }
-                    $techs = explode(',',$this->techs);
-                    foreach($techs as $tech)
-                    {
-                        $techn = User::where('id',$tech)->first();
+                    $techs = explode(',', $this->techs);
+                    foreach ($techs as $tech) {
+                        $techn = User::where('id', $tech)->first();
                         $technician = new \stdClass();
                         $technician->tech_id = $tech;
-                        $technician->tech_fullname = $techn['lastname']." ".$techn['firstname'];
+                        $technician->tech_fullname = $techn['lastname'] . " " . $techn['firstname'];
                         array_push($technicians, $technician);
                     }
                     return $technicians;
                 }),
 
-            "editable" => array([
-            "resource" => "Βλάβη",
-            "id" => $this->id,
-            "info" =>[
-            "client_lastname" => $this->client['lastname'],
-            "client_firstname" => $this->client['firstname'],
-            "client_address" => $this->client['address'].",".$this->client['location'].",".$this->client['zipcode'],
-            "client_phone" =>  $this->when(true, function () {
-                if($this->client['telephone'] != null) return $this->client['telephone'];
-                if($this->client['telephone2'] != null) return $this->client['telephone2'];
-                if($this->client['mobile'] != null) return $this->client['mobile'];
-            })],
-            "damage" => ["field"=>"damage_type_id", "value" => $this->damage_type_id, "type" => "search", "title" => "Τύπος βλάβης" , "page"=>"damagetypes", "holder"=>$this->type['name'], "required" => true],
-            "damage_comments" => ["field"=>"damage_comments","value" => $this->damage_comments, "type" => "text", "title" => "Σχόλια Βλάβης", "required" => false],
-            "cost" => ["field"=>"cost","value" => $this->cost, "type" => "float", "title" => "Τιμή", "required" => false],
-            //"cost" => $this->cost,
-            "guarantee" => ["field"=>"guarantee","value" => $this->guarantee, "type" => "boolean", "title" => "Εγγύηση", "radioItems"=> [[ "id" => "1", "title" => "Με εγγύηση" ], [ "id" => 0, "title" => "Χωρίς εγγύηση"]],"required" => true],
-            //"guarantee" => $this->guarantee,
-            //"status" => $this->status,
-            "status" => ["field" => "status", "value" => $this->status, "type" => "boolean", "title" => "Κατάσταση", "required" => true],
-            //"appointment_pending" => $this->appointment_pending,
-            "appointment_pending" => ["required" => true, "field"=>"appointment_pending","value" => $this->appointment_pending, "type" => "boolean", "title" => "Αναμονή ραντεβού", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"technician_left" => $this->technician_left,
-            "technician_left" => ["required" => true, "field"=>"technician_left","value" => $this->technician_left, "type" => "boolean", "title" => "Αποχώρηση Τεχνικού", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"technician_arrived" => $this->technician_arrived,
-            "technician_arrived" => ["required" => true, "field"=>"technician_arrived","value" => $this->technician_arrived, "type" => "boolean", "title" => "Άφηξη Τεχνικού", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"appointment_completed" => $this->appointment_completed,
-            "appointment_completed" => ["required" => true, "field"=>"appointment_completed","value" => $this->appointment_completed, "type" => "boolean", "title" => "Ολοκλήρωση Ραντεβού", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"appointment_needed" => $this->appointment_needed,
-            "appointment_needed" => ["required" => true, "field"=>"appointment_needed","value" => $this->appointment_needed, "type" => "boolean", "title" => "Ανάγκη για Νέο Ραντεβού", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"supplement_pending" => $this->supplement_pending,
-            "supplement_pending" => ["required" => true, "field"=>"supplement_pending","value" => $this->supplement_pending, "type" => "boolean", "title" => "Αναμονή Ανταλλακτικού", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"damage_fixed" => $this->damage_fixed,
-            "damage_fixed" => ["required" => true, "field"=>"damage_fixed","value" => $this->damage_fixed, "type" => "boolean", "title" => "Βλάβη Επιδιορθώθηκε", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"completed_no_transaction" => $this->completed_no_transaction,
-            "completed_no_transaction" => ["required" => true, "field"=>"completed_no_transaction", "value" => $this->completed_no_transaction, "type" => "boolean", "title" => "Ολοκλήρωση χωρίς συναλλαγή", "radioItems"=> [[ "id" => "0", "title" => "Οχι" ], [ "id" => 1, "title" => "Ναι"]]],
-            //"client_id" => $this->client_id,
-            "client" => ["required" => true, "field"=>"client_id", "value" => $this->client_id, "type" => "search", "title" => "Πελάτης", "page"=>"clients", "holder"=>$this->client['firstname']." ".$this->client['lastname']." | ". $this->client['address']],
-            // "client_lastname" => $this->client['lastname'],
-            // "client_firstname" => $this->client['firstname'],
-            //"client_address" => $this->client['address'],
-            // "client_phone" =>  $this->when(true, function () {
-            //     if($this->client['telephone'] != null) return $this->client['telephone'];
-            //     if($this->client['telephone2'] != null) return $this->client['telephone2'];
-            //     if($this->client['mobile'] != null) return $this->client['mobile'];
-            // }),
-            // "manufacturer_id" => $this->manufacturer_id,
-            "manufacturer" => ["required" => true, "field"=>"manufacturer_id", "value" => $this->manufacturer_id],
-            // "manufacturer" => $this->device['mark']['manufacturer']['name'],
-            //"mark_id" => $this->mark_id,
-            "mark" => ["required" => true, "field"=>"mark_id", "value" => $this->mark_id],
-            //"device_id" => $this->device_id,
-            //"device" => $this->device['name'],
-            "devices" => ["required" => true, "field"=>"device_id", "value" => $this->device_id, "type" => "searchdevices", "title" => "Συσκευή", "page"=>"devices", "holder"=> $this->device['mark']['manufacturer']['name']." / ".$this->device['mark']['name']." / ".$this->device['name']],
-            "supplement" => ["required" => false, "field" => "supplement" , "title" => "Ανταλλακτικά", "type" => "text", "value" => $this->supplement],
-            "comments" =>  ["required" => false, "field" => "comments", "type" => "text", "title" => "Γενικά Σχόλια", "value" => $this->comments],
-            //"appointment_start" => $this->appointment_start,
-            "appointment_start" => ["required" => false, "field" => "appointment_start", "title" => "Έναρξη Ραντεβού" , "type" =>"datetime", "value" => $this->appointment_start],
-            //"appointment_end" => $this->appointment_end,
-            "appointment_end" => ["required" => false, "field" => "appointment_end", "title" => "Λήξη Ραντεβού" ,"type" =>"datetime", "value" => $this->appointment_end],
-            "techs" => $this->when(true,function()
-            {
-                $technicians = array();
-                $technician_ids = array();
-                if($this->techs != null)
-                {
-                    $techs = explode(',',$this->techs);
-                    foreach($techs as $tech)
-                    {
-                        $techn = User::where('id',$tech)->first();
-                        array_push($technicians, $techn['lastname']." ".$techn['firstname']);
-                        array_push($technician_ids, $tech);
-                    }
+                "editable" => array([
+                    "resource" => "Βλάβη",
+                    "id" => $this->id,
+                    "info" => [
+                        "client_lastname" => $this->client['lastname'],
+                        "client_firstname" => $this->client['firstname'],
+                        "client_address" => $this->client['address'] . "," . $this->client['location'] . "," . $this->client['zipcode'],
+                        "client_phone" =>  $this->when(true, function () {
+                            if ($this->client['telephone'] != null) return $this->client['telephone'];
+                            if ($this->client['telephone2'] != null) return $this->client['telephone2'];
+                            if ($this->client['mobile'] != null) return $this->client['mobile'];
+                        })
+                    ],
+                    "damage" => ["field" => "damage_type_id", "value" => $this->damage_type_id, "type" => "search", "title" => "Τύπος βλάβης", "page" => "damagetypes", "holder" => $this->type['name']],
+                    "client" => ["field" => "client_id", "value" => $this->client_id, "type" => "search", "title" => "Πελάτης", "page" => "clients", "holder" => $this->client['firstname'] . " " . $this->client['lastname'] . " | " . $this->client['address'], "required" => true],
+                    "techs" => $this->when(true, function () {
+                        $technicians = array();
+                        $technician_ids = array();
+                        if ($this->techs != null) {
+                            $techs = explode(',', $this->techs);
+                            foreach ($techs as $tech) {
+                                $techn = User::where('id', $tech)->first();
+                                array_push($technicians, $techn['lastname'] . " " . $techn['firstname']);
+                                array_push($technician_ids, $tech);
+                            }
 
-                    $technicians = ["required" => false, "title" => "Τεχνικοί", "field" => "techs" , "type" => "searchtechs", "page" => "tech", "value" => $technician_ids, "holder" => $technicians];
+                            $technicians = ["title" => "Τεχνικοί", "field" => "techs", "type" => "searchtechs", "page" => "tech", "value" => $technician_ids, "holder" => $technicians, "required" => false];
+                        }
 
-                }
-
-                return $technicians;
-
-            })
-            //"user" => $this->user['lastname']
-                ])];
+                        return $technicians;
+                    }),
+                    "manufacturer" => ["field" => "manufacturer_id", "value" => $this->manufacturer_id, "required" => true],
+                    "mark" => ["field" => "mark_id", "value" => $this->mark_id, "required" => true],
+                    "devices" => ["field" => "device_id", "value" => $this->device_id, "type" => "searchdevices", "title" => "Συσκευή", "page" => "devices", "holder" => $this->device['mark']['manufacturer']['name'] . " / " . $this->device['mark']['name'] . " / " . $this->device['name'], "required" => true],
+                    "status" => ["field" => "status", "value" => $this->status, "type" => "boolean", "title" => "Κατάσταση", "radioItems" => [["id" => "Ολοκληρωμένη", "title" => "Ολοκληρωμένη"], ["id" => "Μη Ολοκληρωμένη", "title" => "Μη Ολοκληρωμένη"], ["id" => "Ακυρώθηκε", "title" => "Ακυρώθηκε"]], "required" => true],
+                    "guarantee" => ["field" => "guarantee", "value" => $this->guarantee, "type" => "boolean", "title" => "Εγγύηση", "radioItems" => [["id" => "1", "title" => "Με εγγύηση"], ["id" => 0, "title" => "Χωρίς εγγύηση"]], "required" => true],
+                    "appointment_pending" => ["field" => "appointment_pending", "value" => $this->appointment_pending, "type" => "boolean", "title" => "Αναμονή ραντεβού", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "technician_left" => ["field" => "technician_left", "value" => $this->technician_left, "type" => "boolean", "title" => "Αποχώρηση Τεχνικού", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "technician_arrived" => ["field" => "technician_arrived", "value" => $this->technician_arrived, "type" => "boolean", "title" => "Άφηξη Τεχνικού", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "appointment_completed" => ["field" => "appointment_completed", "value" => $this->appointment_completed, "type" => "boolean", "title" => "Ολοκλήρωση Ραντεβού", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "appointment_needed" => ["field" => "appointment_needed", "value" => $this->appointment_needed, "type" => "boolean", "title" => "Ανάγκη για Νέο Ραντεβού", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "supplement_pending" => ["field" => "supplement_pending", "value" => $this->supplement_pending, "type" => "boolean", "title" => "Αναμονή Ανταλλακτικού", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "damage_fixed" => ["field" => "damage_fixed", "value" => $this->damage_fixed, "type" => "boolean", "title" => "Βλάβη Επιδιορθώθηκε", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "completed_no_transaction" => ["field" => "completed_no_transaction", "value" => $this->completed_no_transaction, "type" => "boolean", "title" => "Ολοκλήρωση χωρίς συναλλαγή", "radioItems" => [["id" => "0", "title" => "Οχι"], ["id" => 1, "title" => "Ναι"]], "required" => true],
+                    "appointment_start" => ["field" => "appointment_start", "title" => "Έναρξη Ραντεβού", "type" => "datetime", "value" => $this->appointment_start, "required" => false],
+                    "appointment_end" => ["field" => "appointment_end", "title" => "Λήξη Ραντεβού", "type" => "datetime", "value" => $this->appointment_end, "required" => false],
+                    "cost" => ["field" => "cost", "value" => $this->cost, "type" => "float", "title" => "Τιμή", "required" => false],
+                    "supplement" => ["field" => "supplement", "title" => "Ανταλλακτικά", "type" => "text", "value" => $this->supplement, "required" => false],
+                    "damage_comments" => ["field" => "damage_comments", "value" => $this->damage_comments, "type" => "text", "title" => "Σχόλια Βλάβης", "required" => false],
+                    "comments" =>  ["field" => "comments", "type" => "text", "title" => "Γενικά Σχόλια", "value" => $this->comments, "required" => false]
+                ])
+            ];
     }
 }
