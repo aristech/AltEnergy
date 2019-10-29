28be41a0f02dae93c82fa416dcf6e4e45dd109ee
GManentis <manentis.gerasimos@outlook.com>
2019-10-29 16:35:30 +0200

    Changes in Note Resource, added Documentation on Notes Model

--- Laravel/app/Http/Resources/NoteResource.php
+++ Laravel/app/Http/Resources/NoteResource.php
@@ -39,9 +39,10 @@ class NoteResource extends JsonResource
                 "id" => $this->id,
                 "title" =>  ["field" => "title", "type" => "text", "title" => "Τίτλος Σημείωσης", "value" => $this->title, "required" => true],
                 "description" =>  ["field" => "description", "type" => "text", "title" => "Σημείωση", "value" => $this->description, "required" => true],
+                "importance" => ["field" => "all_day", "value" => $this->all_day, "type" => "boolean", "title" => "Σημαντικότητα", "radioItems" => [["id" => 0, "title" => "Υψηλή"], ["id" => 1, "title" => "Μεσαία"], ["id" => 2, "title" => "Χαμηλή"]], "required" => true],
                 "all_day" => ["field" => "all_day", "value" => $this->all_day, "type" => "boolean", "title" => "Όλη μέρα", "radioItems" => [["id" => 1, "title" => "Ναι"], ["id" => 0, "title" => "Όχι"]], "required" => true],
-                "dateTime_start" => ["field" => "dateTime_start", "title" => "Έναρξη", "type" => "datetime", "value" => $this->dateTime_start, "required" => false],
-                "dateTime_end" => ["field" => "dateTime_end", "title" => "Λήξη", "type" => "datetime", "value" => $this->dateTime_end, "required" => false]
+                "dateTime_start" => ["field" => "dateTime_start", "title" => "Έναρξη", "type" => "datetime", "value" => $this->dateTime_start, "required" => true],
+                "dateTime_end" => ["field" => "dateTime_end", "title" => "Λήξη", "type" => "datetime", "value" => $this->dateTime_end, "required" => true]
             ])
         ];
     }
