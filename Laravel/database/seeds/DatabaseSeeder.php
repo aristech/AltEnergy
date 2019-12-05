b5dbcf96012cb4f39e0fd03acbb7957d39d72f07
GManentis <manentis.gerasimos@outlook.com>
2019-12-05 13:19:24 +0200

    Created seeder for super admin

--- Laravel/database/seeds/DatabaseSeeder.php
+++ Laravel/database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Role;
+use App\UsersRoles;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,10 +19,13 @@ class DatabaseSeeder extends Seeder
         // DB::table('roles')->insert(["title"=>"Τεχνικός"]);
         // DB::table('roles')->insert(["title"=>"Admin"]);
         // DB::table('roles')->insert(["title"=>"Super Admin"]);
-        factory(App\Manufacturer::class,5)->create();
-        factory(App\Mark::class,5)->create();
-        factory(App\Device::class,10)->create();
-        factory(App\DamageType::class,5)->create();
-        factory(App\Damage::class,25)->create();
+        // factory(App\Manufacturer::class,5)->create();
+        // factory(App\Mark::class,5)->create();
+        // factory(App\Device::class,10)->create();
+        // factory(App\DamageType::class,5)->create();
+        // factory(App\Damage::class,25)->create();
+        DB::table('users')->insert(["firstname" => "TestAdmin", "lastname" => "Progressnet", "email" => "testadmin@progressnet.gr", "password" => bcrypt("progressnet"), "telephone" => "2111828724", "active" => true]);
+        $admin_id = DB::table('users')->where('email', 'testadmin@progressnet.gr')->value('id');
+        DB::table('role_user')->insert(["role_id" => 5, "user_id" => $admin_id]);
     }
 }
