03e36f4e9e09cdf44931d96d2565196bddbed54b
GManentis <manentis.gerasimos@outlook.com>
2019-12-16 12:11:18 +0200

    Changes in notes resource for consistency

--- Laravel/app/Http/Resources/NoteResource.php
+++ Laravel/app/Http/Resources/NoteResource.php
@@ -36,12 +36,18 @@ class NoteResource extends JsonResource
                 "editable" => array([
                     "resource" => "notes",
                     "id" => $this->id,
-                    "title" =>  ["field" => "title", "type" => "text", "title" => "Τίτλος Σημείωσης", "value" => $this->title, "required" => true],
-                    "description" =>  ["field" => "description", "type" => "text", "title" => "Σημείωση", "value" => $this->description, "required" => true],
-                    "importance" => ["field" => "importance", "value" => $this->importance, "type" => "boolean", "title" => "Σημαντικότητα", "radioItems" => [["id" => 0, "title" => "Υψηλή"], ["id" => 1, "title" => "Μεσαία"], ["id" => 2, "title" => "Χαμηλή"]], "required" => true],
-                    "all_day" => ["field" => "all_day", "value" => $this->all_day, "type" => "boolean", "title" => "Όλη μέρα", "radioItems" => [["id" => 1, "title" => "Ναι"], ["id" => 0, "title" => "Όχι"]], "required" => true],
-                    "dateTime_start" => ["field" => "dateTime_start", "title" => "Έναρξη", "type" => "datetime", "value" => $this->dateTime_start, "required" => true],
-                    "dateTime_end" => ["field" => "dateTime_end", "title" => "Λήξη", "type" => "datetime", "value" => $this->dateTime_end, "required" => true]
+                    "info" => [
+                        "client_lastname" => "NA",
+                        "client_firstname" => "NA",
+                        "client_address" => "NA",
+                        "client_phone" =>  "NA"
+                    ],
+                    "title" =>  ["roles" => array(5, 4, 3), "field" => "title", "type" => "text", "title" => "Τίτλος Σημείωσης", "value" => $this->title, "required" => true],
+                    "description" =>  ["roles" => array(5, 4, 3), "field" => "description", "type" => "text", "title" => "Σημείωση", "value" => $this->description, "required" => true],
+                    "importance" => ["roles" => array(5, 4, 3), "field" => "importance", "value" => $this->importance, "type" => "boolean", "title" => "Σημαντικότητα", "radioItems" => [["id" => 0, "title" => "Υψηλή"], ["id" => 1, "title" => "Μεσαία"], ["id" => 2, "title" => "Χαμηλή"]], "required" => true],
+                    "all_day" => ["roles" => array(5, 4, 3), "field" => "all_day", "value" => $this->all_day, "type" => "boolean", "title" => "Όλη μέρα", "radioItems" => [["id" => 1, "title" => "Ναι"], ["id" => 0, "title" => "Όχι"]], "required" => true],
+                    "dateTime_start" => ["roles" => array(5, 4, 3), "field" => "dateTime_start", "title" => "Έναρξη", "type" => "datetime", "value" => $this->dateTime_start, "required" => true],
+                    "dateTime_end" => ["roles" => array(5, 4, 3), "field" => "dateTime_end", "title" => "Λήξη", "type" => "datetime", "value" => $this->dateTime_end, "required" => true]
                 ])
             ];
     }
